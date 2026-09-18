cac788c03352611beb493e68b3da4c6354c24061
ritesh <riteshsahoo212121@gmail.com>
2026-09-18 19:19:29 +0530

    Remove temporary deploy script

--- deploy_video_fix.ps1
+++
@@ -1,7 +0,0 @@
-$askpass = "$env:TEMP\askpass.bat"
-"@echo Yoga@1431430" | Out-File -FilePath $askpass -Encoding ascii
-$env:SSH_ASKPASS = $askpass
-$env:SSH_ASKPASS_REQUIRE = "force"
-$env:DISPLAY = "dummy:0"
-ssh -o StrictHostKeyChecking=no root@191.215.37.241 "curl -I https://grandstore.co.za/assets/media/grand-store-hero-scrub.mp4"
-Remove-Item -Force $askpass
